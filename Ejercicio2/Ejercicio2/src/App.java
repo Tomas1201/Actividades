f00562780ffda10ba9187a5cdb7252423718d052
Tomas Acosta <109447863+Tomas1201@users.noreply.github.com>
2023-09-07 16:37:47 -0300

    Actividad 2

---
+++ Ejercicio2/Ejercicio2/src/App.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+public class App {
+static Stack<Integer> pila = new Stack<Integer>();
+  static  int valorviejo = 2;
+   static int valornuevo = 45;
+    public static void main(String[] args) throws Exception {
+     
+    }
+
+    public static int remplazar(Stack pila, int valornuevo, int valorviejo){
+
+return 23;
+    }
+}
